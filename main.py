470fb48364d33a05146898fa2ce15e7b11434e11
AlessandroPerelli <alessandroperelli156@gmail.com>
2023-04-10 20:48:23 +0100

    added player to screen

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        # load player sprite
-        self.image = pygame.image.load("sprites/player/dinosaur/run1.png")
-        self.rect = self.image.get_rect()
-        # initial position
-        self.rect.x = x
-        self.rect.y = y
-        # initial speed
-        self.speed = 5
+  def __init__(self, x, y):
+    super().__init__()
+    # load player sprite
+    self.image = pygame.image.load("sprites/player/dinosaur/run1.png")
+    self.rect = self.image.get_rect()
+    # initial position
+    self.rect.x = x
+    self.rect.y = y
+    # initial speed
+    self.speed = 5
 
-    def update(self):
-      key = pygame.key.get_pressed()
-      if key[pygame.K_UP]:
-        self.rect.y -= self.speed
-      if key[pygame.K_DOWN]:
-        self.rect.y += self.speed
+  def update(self):
+    key = pygame.key.get_pressed()
+    if key[pygame.K_UP]:
+      self.rect.y -= self.speed
+    if key[pygame.K_DOWN]:
+      self.rect.y += self.speed
 
 # initialise
 pygame.init()
@@ -29,25 +29,34 @@ pygame.display.set_caption("Dino Run")
 # set the background color
 background_color = (255, 255, 255)
 
+# player initialisation
+player = Player(100, 100)
+player_group = pygame.sprite.Group()
+player_group.add(player)
+
 # set the clock
 clock = pygame.time.Clock()
 
 # main loop
 running = True
 while running:
-    # handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    # clear
-    screen.fill(background_color)    
-    
-    # update
-    pygame.display.update()
-    
-    # wait for the next frame
-    clock.tick(60)
+  # handle events
+  for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          running = False
+  
+  # clear
+  screen.fill(background_color)    
+  
+  # update player sprite
+  player.update()
+  player_group.draw(screen)
+
+  # update
+  pygame.display.update()
+  
+  # wait for the next frame
+  clock.tick(60)
 
 # quit 
 pygame.quit()
