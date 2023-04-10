822acb3a95ab66e602aa947fcfb8999d78f4e888
AlessandroPerelli <alessandroperelli156@gmail.com>
2023-04-10 19:55:42 +0100

    added sprite assets

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pygame
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.sprite_sheet = pygame.image.load("sprites/dinosaur_player.png")
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        pass
+
 # initialise
 pygame.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("My Pygame")
+pygame.display.set_caption("Dino Run")
+
+player_sprite = pygame.image.load("assets/dino.png")
 
 # set the background color
 background_color = (255, 255, 255)
